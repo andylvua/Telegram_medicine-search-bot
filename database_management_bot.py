f683b5da2c0efb3421a5fb24ae24399ee5b96569
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-05-15 03:24:53 +0300

    Changed logging format

--- database_management_bot.py
+++ database_management_bot.py
@@ -21,7 +21,8 @@ from pymongo import MongoClient
 import validators
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO
+    format='%(asctime)s.%(msecs)03d - %(name)s - %(funcName)s() - %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 

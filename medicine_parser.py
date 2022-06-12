c6afbda80de4b324a932ca5bc3b2630b9d9f7ef8
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-06-12 18:45:09 +0300

    Added missing type hinting and docstring

--- medicine_parser.py
+++ medicine_parser.py
@@ -8,19 +8,42 @@ import cloudscraper
 from googletrans import Translator
 
 
-def is_cyrillic(query_string):
+def is_cyrillic(query_string: str) -> bool:
+    """
+    The is_cyrillic function checks if the query string contains any Cyrillic characters.
+    If it does, True is returned. Otherwise, False is returned.
+
+    :param query_string: Check if the query string contains any cyrillic characters
+    :return: True if the query_string contains any cyrillic characters, and false otherwise
+    """
     if regex.search(r'\p{IsCyrillic}', query_string):
         return True
     else:
         return False
 
 
-def translate(query_string):
+def translate(query_string: str) -> str:
+    """
+    Takes a string as input and returns the translated string in ukrainian.
+
+    :param query_string: Pass the string to be translated as a parameter
+    :return: A translation of the query string
+    """
     translator = Translator()
     return translator.translate(query_string, dest='uk').text
 
 
-def find_info(query_string):
+def find_info(query_string: str) -> dict or None:
+    """
+    Takes a string as an argument parses website and returns a dictionary with the following keys:
+        name - medicine name
+        active_ingredient - active ingredient of the medicine
+        pharmgroup - pharmacological group of the medicine (e.g., analgesics, antiseptics)
+        indication - medical indication
+
+    :param query_string: Pass the name of the medicine to find
+    :return: A dictionary with the following keys: name, active_ingredient, pharmgroup, indication and contraindication
+    """
     if not is_cyrillic(query_string):
         query_string = translate(query_string)
 
@@ -82,7 +105,27 @@ def find_info(query_string):
     return info
 
 
-def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end=""):
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="") -> None:
+    """
+    The print_progress_bar function prints a progress bar to the console.
+
+    Args:
+        iteration (int): The current iteration.
+        total (int): The total number of iterations before completion.
+
+        prefix (str, optional): A string that will be prepended to the progress bar and any
+        associated text during printing.
+
+    :param iteration: Keep track of the current iteration
+    :param total: Determine the total number of iterations necessary to fill the bar
+    :param prefix: Print a string before the progress bar
+    :param suffix: Print a message after the progress bar
+    :param decimals: Specify the number of decimal places to display in the output
+    :param length: Determine the length of the progress bar
+    :param fill: Determine the character used to fill the bar
+    :param print_end: Prevent the print function from printing a new line after each iteration
+    :return: None
+    """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     bar = fill * filled_length + '-' * (length - filled_length)

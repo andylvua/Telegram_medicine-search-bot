1e94ac75e59caa786a20dcf60bb0e418ba6c9e0c
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-06-21 23:42:49 +0300

    Improved module structure to make code more readable and safe

--- modules/medicine_parser.py
+++ modules/medicine_parser.py
@@ -1,4 +1,4 @@
-# import json
+import json
 import re
 
 import bs4
@@ -70,45 +70,63 @@ def find_info_tabletki_ua(query_string: str) -> dict or None:
 
     medicine_name = search_result["title"]
     medicine_page_link = 'https://tabletki.ua' + search_result["href"]
+
     medicine_photo_link = search_result.find("img")["src"]
-
     medicine_photo = requests.get(medicine_photo_link).content
-
-    # image = Image.open(io.BytesIO(medicine_photo))
-    # image.show()
 
     medicine_page = scraper.get(medicine_page_link)
 
     medicine = bs4.BeautifulSoup(medicine_page.text, "html.parser")
 
-    try:
-        medicine_active_ingredient = medicine.find("div", {"id": "instr_cont_0"}).find("p").text \
-            .split(":")[-1].strip().strip(";").capitalize()
-    except AttributeError:
-        medicine_active_ingredient = None
+    def parse_active_ingredient(_medicine):
+        try:
+            _medicine_active_ingredient = medicine.find("div", {"id": "instr_cont_0"}).find("p").text \
+                .split(":")[-1].strip().strip(";").capitalize()
+            return _medicine_active_ingredient
+        except AttributeError:
+            _medicine_active_ingredient = "Не знайдено"
 
-    try:
-        medicine_pharmgroup = medicine.find("div", {"id": "instr_cont_2"}).find("p").text.strip()
-    except AttributeError:
-        medicine_pharmgroup = None
+        return _medicine_active_ingredient
 
-    try:
-        medicine_indication_list = medicine.find("div", {"id": "instr_cont_4"}).text.split(".")[0].strip().split(";")
-    except AttributeError:
-        medicine_indication = None
-    else:
-        medicine_indication_list = [indication.capitalize() for indication in medicine_indication_list]
+    def parse_pharmgroup(_medicine):
+        try:
+            _medicine_pharmgroup = medicine.find("div", {"id": "instr_cont_2"}).find("p").text.strip()
+            return _medicine_pharmgroup
+        except AttributeError:
+            _medicine_pharmgroup = "Не знайдено"
 
-        medicine_indication = ''
-        for indication in medicine_indication_list:
-            medicine_indication += indication + ".\n"
+        return _medicine_pharmgroup
 
-        medicine_indication = medicine_indication.strip()
+    def parse_indication(_medicine):
+        try:
+            medicine_indication_list = medicine.find("div", {"id": "instr_cont_4"}).text.split(".")[0].strip().split(
+                ";")
+        except AttributeError:
+            _medicine_indication = "Не знайдено"
+        else:
+            medicine_indication_list = [indication.capitalize() for indication in medicine_indication_list]
 
-    try:
-        medicine_contraindication = medicine.find("div", {"id": "instr_cont_5"}).text.split(".")[0].strip()
-    except AttributeError:
-        medicine_contraindication = None
+            _medicine_indication = ''
+            for indication in medicine_indication_list:
+                _medicine_indication += indication + ".\n"
+
+            _medicine_indication = _medicine_indication.strip()
+
+        return _medicine_indication
+
+    def parse_contraindication(_medicine):
+        try:
+            _medicine_contraindication = medicine.find("div", {"id": "instr_cont_5"}).text.split(".")[0].strip()
+            return _medicine_contraindication
+        except AttributeError:
+            _medicine_contraindication = "Не знайдено"
+
+        return _medicine_contraindication
+
+    medicine_active_ingredient = parse_active_ingredient(medicine)
+    medicine_pharmgroup = parse_pharmgroup(medicine)
+    medicine_indication = parse_indication(medicine)
+    medicine_contraindication = parse_contraindication(medicine)
 
     info = {
         "link": medicine_page_link,
@@ -132,6 +150,72 @@ def find_info_drug_control(query_string):
     :param query_string: Pass the name of the medicine to find
     :return: A dictionary with the following keys: name, active_ingredient, pharmgroup, indication and contraindication
     """
+
+    def parse_active_ingredient(_medicine):
+        try:
+            _medicine_active_ingredient = medicine.find(
+                "a",
+                string=re.compile('діюча речовина', re.IGNORECASE),
+            ).parent.text.split(":")[-1].strip().strip(";").capitalize()
+            return _medicine_active_ingredient
+        except AttributeError:
+            _medicine_active_ingredient = "Не знайдено"
+
+        return _medicine_active_ingredient
+
+    def parse_pharmgroup(_medicine):
+        try:
+            _medicine_pharmgroup = medicine.find(
+                "h2",
+                string=re.compile('група', re.IGNORECASE)
+            ).next_sibling.text.strip(". ")
+            return _medicine_pharmgroup
+        except AttributeError:
+            pass
+
+        try:
+            _medicine_pharmgroup = medicine.find(
+                "h2",
+                string=re.compile('група', re.IGNORECASE)
+            ).find_next().text.strip(". ")
+            return _medicine_pharmgroup
+        except AttributeError:
+            _medicine_pharmgroup = "Не знайдено"
+
+        return _medicine_pharmgroup
+
+    def parse_indication(_medicine):
+        try:
+            _medicine_indication = medicine.find(
+                "b",
+                string=re.compile('показання', re.IGNORECASE),
+            ).next_sibling.text.strip(". ")
+            return _medicine_indication
+        except AttributeError:
+            pass
+
+        try:
+            _medicine_indication = medicine.find(
+                "b",
+                string=re.compile('показання', re.IGNORECASE),
+            ).find_next().text.strip(". ")
+            return _medicine_indication
+        except AttributeError:
+            _medicine_indication = "Не знайдено"
+
+        return _medicine_indication
+
+    def parse_contraindication(_medicine):
+        try:
+            _medicine_contraindication = medicine.find(
+                "b",
+                string=re.compile('протипоказання', re.IGNORECASE),
+            ).find_next().find_next().text.split(".")[0].strip("-; ")
+        except AttributeError:
+            _medicine_contraindication = "Не знайдено"
+
+        return _medicine_contraindication
+
     if not is_cyrillic(query_string):
         query_string = translate(query_string)
 
@@ -157,14 +241,7 @@ def find_info_drug_control(query_string):
     medicine_active_ingredient = parse_active_ingredient(medicine)
     medicine_pharmgroup = parse_pharmgroup(medicine)
     medicine_indication = parse_indication(medicine)
-
-    try:
-        medicine_contraindication = medicine.find(
-            "b",
-            string=re.compile('протипоказання', re.IGNORECASE),
-        ).find_next().find_next().text.split(".")[0].strip("-; ")
-    except AttributeError:
-        medicine_contraindication = "Не знайдено"
+    medicine_contraindication = parse_contraindication(medicine)
 
     info = {
         "link": medicine_page_link,
@@ -175,63 +252,6 @@ def find_info_drug_control(query_string):
         "contrandication": medicine_contraindication
     }
     return info
-
-
-def parse_active_ingredient(medicine):
-    try:
-        medicine_active_ingredient = medicine.find(
-            "a",
-            string=re.compile('діюча речовина', re.IGNORECASE),
-        ).parent.text.split(":")[-1].strip().strip(";").capitalize()
-        return medicine_active_ingredient
-    except AttributeError:
-        medicine_active_ingredient = "Не знайдено"
-
-    return medicine_active_ingredient
-
-
-def parse_pharmgroup(medicine):
-    try:
-        medicine_pharmgroup = medicine.find(
-            "h2",
-            string=re.compile('група', re.IGNORECASE)
-        ).next_sibling.text.strip(". ")
-        return medicine_pharmgroup
-    except AttributeError:
-        pass
-    
-    try:
-        medicine_pharmgroup = medicine.find(
-            "h2",
-            string=re.compile('група', re.IGNORECASE)
-        ).find_next().text.strip(". ")
-        return medicine_pharmgroup
-    except AttributeError:
-        medicine_pharmgroup = "Не знайдено"
-
-    return medicine_pharmgroup
-
-
-def parse_indication(medicine):
-    try:
-        medicine_indication = medicine.find(
-            "b",
-            string=re.compile('показання', re.IGNORECASE),
-        ).next_sibling.text.strip(". ")
-        return medicine_indication
-    except AttributeError:
-        pass
-
-    try:
-        medicine_indication = medicine.find(
-            "b",
-            string=re.compile('показання', re.IGNORECASE),
-        ).find_next().text.strip(". ")
-        return medicine_indication
-    except AttributeError:
-        medicine_indication = "Не знайдено"
-
-    return medicine_indication
 
 
 def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="") -> None:
@@ -264,31 +284,32 @@ def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, lengt
         print()
 
 
-# with open('names.txt') as file:
-#     names_lines = file.readlines()
-#
-# info_list = list()
-#
-# print_progress_bar(0, len(names_lines), prefix='Progress:', suffix='Complete', length=50)
-#
-# found = 0
-# for i, name in enumerate(names_lines):
-#     data = find_info(name)
-#
-#     if data and data["active_ingredient"]:
-#         medicine_info = {
-#             "Назва": data["name"],
-#             "Діюча речовина": data["active_ingredient"],
-#             "Фармгрупа": data["pharmgroup"],
-#             "Показання": data["indication"],
-#             "Протипоказання": data["contrandication"]
-#         }
-#         found += 1
-#         info_list.append(medicine_info)
-#
-#     print_progress_bar(i + 1, len(names_lines), prefix='Progress:', suffix='Complete', length=50)
-#
-# with open("medicine_info.json", "w", encoding='utf-8') as final:
-#     json.dump(info_list, final, sort_keys=False, ensure_ascii=False, indent=4)
-#
-# print(f"Done. Found {found} out of {len(names_lines)} medicines")
+def parser():
+    with open('names.txt') as file:
+        names_lines = file.readlines()
+
+    info_list = list()
+
+    print_progress_bar(0, len(names_lines), prefix='Progress:', suffix='Complete', length=50)
+
+    found = 0
+    for i, name in enumerate(names_lines):
+        data = find_info_tabletki_ua(name)
+
+        if data and data["active_ingredient"]:
+            medicine_info = {
+                "Назва": data["name"],
+                "Діюча речовина": data["active_ingredient"],
+                "Фармгрупа": data["pharmgroup"],
+                "Показання": data["indication"],
+                "Протипоказання": data["contrandication"]
+            }
+            found += 1
+            info_list.append(medicine_info)
+
+        print_progress_bar(i + 1, len(names_lines), prefix='Progress:', suffix='Complete', length=50)
+
+    with open("medicine_info.json", "w", encoding='utf-8') as final:
+        json.dump(info_list, final, sort_keys=False, ensure_ascii=False, indent=4)
+
+    print(f"Done. Found {found} out of {len(names_lines)} medicines")
